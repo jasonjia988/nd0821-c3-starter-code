650bee9208abbad9da2bcd64570ae4bfd8576ec7
jason <jiayanzhao988@gmail.com>
2025-12-10 21:11:41 +0100

    fix flake8 v12

--- starter/test_API.py
+++ starter/test_API.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_get_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == "Hello World!"
+    assert response.json() == "Weclome to use this API!"
 
 
 def test_inference_response_schema():
